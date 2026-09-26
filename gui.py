5ccecfe9cf14f4360e872cceb3e1f573059c9ca4
mridulnag123-svg <mridulnag123@gmail.com>
2026-09-26 20:01:37 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -5979,9 +5979,5 @@ if __name__ == "__main__":
         server_name="0.0.0.0",
         server_port=7861,
         inbrowser=False,
-<<<<<<< HEAD
         share=True
     )
-=======
-    )
->>>>>>> 59f80a7607990e89c63b7bb0bd6491097089dc9c
